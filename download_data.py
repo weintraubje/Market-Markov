e70e87d2c096191187cf86905bb3ec2ac2b3e702
Jeff Weintraub <32884134+weintraubje@users.noreply.github.com>
2021-04-26 01:11:40 -0400

    Revert "bad_commit"
    This reverts commit da9b328f65954c60748bb94a233e5ff4e63ef9e2.

--- download_data.py
+++ download_data.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
-import yfinance as yf
 
-def download_stock_data(symbol, start, end):
+def download_stock_data(symbol):
     """Downloads historical stock pricing data for an individual symbol"""
 
-    df = pd.DataFrame()
-    
+    df = pd.DataFrame()  
+
     # Print the symbol being downloaded
     print('DOWNLOADING ' + symbol, sep=',', end=' ... ', flush=True)  
 
