38a36cbf5f73eb21fa8013e7d868acae6ac1ca7b
Jeff Weintraub <32884134+weintraubje@users.noreply.github.com>
2021-04-26 00:24:07 -0400

    quick_update_test

---
+++ download_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+def download_stock_data(symbol):
+    """Downloads historical stock pricing data for an individual symbol"""
+
+    df = pd.DataFrame()  
+
+    # Print the symbol being downloaded
+    print('DOWNLOADING ' + symbol, sep=',', end=' ... ', flush=True)  
+
+    try:
+        # Download pricing data
+        stock = []
+        stock = yf.download(symbol, start=start, end=end, progress=False)
+
+        # Append individual prices
+        if len(stock) == 0:
+            None
+        else:
+            stock['Name'] = symbol
+            df = df.append(stock,sort=False)
+            df = df[['Close', 'Name']]
+            print('SUCCESS!')
+            
+    except Exception:
+        print('ERROR IN DOWNLOADING DATA')
+               
+    return df
